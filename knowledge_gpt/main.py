d58c3e7d09081f9f1db4bd75ba73ddbf73fd3497
artem <artyomoskin1@gmail.com>
2024-07-25 16:26:40 +0300

    Github icon removed

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -26,6 +26,13 @@ MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
 
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 st.set_page_config(page_title="RAG_Proto", page_icon="📖", layout="wide")
 st.header("📖RAG_Prototype")
 
